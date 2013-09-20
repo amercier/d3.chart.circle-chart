39b662493b017b3044eacad76f8565530f92c07e
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-09-20 18:01:15 +0200

    WIP on circles

--- example/example.js
+++ example/example.js
@@ -1,3 +1,38 @@
 (function() {
-  // YOUR SAMPLE CHART USAGE GOES HERE.
+
+  // Trim <pre> elements
+  $('pre').each(function(i, el) {
+    var $el = $(el);
+    $el.text( $el.text().trim() );
+  });
+
+  // $('.chart-example').each(function(i, el) {
+  //   var $el = $(el);
+  //   window.eval( $el.find('pre').text() );
+  // });
+
+  (function() {
+    var data = [1,3,4,6,10];
+
+    var chart = d3.select('.chart-example-full .chart-data')
+      .append('svg')
+      .chart('Circles')
+      .width(200)
+      .height(50)
+      .radius(10)
+      .color('#2fa0ec');
+
+    chart.draw(data);
+  })();
+
+  // (function() {
+  //   var data = [1,3,4,6,10];
+
+  //   var chart = d3.select('.chart-example-basic .chart-data')
+  //     .append('svg')
+  //     .chart('Circles');
+
+  //   chart.draw(data);
+  // })();
+
 }());
--- src/circle-chart.js
+++ src/circle-chart.js
@@ -1,5 +1,108 @@
-d3.chart("TemplateChart", {
+d3.chart('Circles', {
+
   initialize: function() {
 
+    console.log(this.__width);
+
+    // defaults options
+    this.__width = this.__width || this.base[0][0].offsetWidth;
+    this.__height = this.__height || 20;
+    this.__radius = this.__radius || 10;
+
+    // create a base scale we will use later.
+    this.xScale = d3.scale.linear();
+
+    this.base
+      .attr('height', '100%')
+      .attr('width', '100%');
+
+    var circlesBase = this.base.append('g')
+        .classed('circles', true)
+        .attr('height', this.__height)
+        .attr('width', this.__width);
+
+    this.layer('circles', circlesBase, {
+
+      dataBind: function(data) {
+
+        // update the domain of the xScale since it depends on the data
+        var chart = this.chart();
+        chart.xScale.domain([Math.min.apply(null, data), Math.max.apply(null, data)]);
+
+        // return a data bound selection for the passed in data.
+        return this.selectAll('circle')
+          .data(data);
+      },
+
+      insert: function() {
+        var chart = this.chart();
+
+        // update the range of the xScale
+        chart.xScale.range([Math.ceil(chart.__radius), chart.__width-Math.ceil(chart.__radius)]);
+
+        // setup the elements that were just created
+        return this.append('circle')
+
+        // setup the non data driven attributes
+          .classed('circle', true)
+          .style('fill', chart.color)
+          .attr('cy', chart.__height/2)
+          .attr('r', chart.__radius);
+      },
+
+      // setup an enter event for the data as it comes in:
+      events: {
+        'enter' : function() {
+          var chart = this.chart();
+          return this.attr('cx', function(d) {
+            return chart.xScale(d);
+          });
+        }
+      }
+    });
+  },
+
+  // configures the width of the chart.
+  // when called without arguments, returns the
+  // current width.
+  width: function(newWidth) {
+    if (arguments.length === 0) {
+      return this.__width;
+    }
+    this.__width = newWidth;
+    return this;
+  },
+
+  // configures the height of the chart.
+  // when called without arguments, returns the
+  // current height.
+  height: function(newHeight) {
+    if (arguments.length === 0) {
+      return this.__height;
+    }
+    this.__height = newHeight;
+    return this;
+  },
+
+  // configures the radius of the circles in the chart.
+  // when called without arguments, returns the
+  // current radius.
+  radius: function(newRadius) {
+    if (arguments.length === 0) {
+      return this.__radius;
+    }
+    this.__radius = newRadius;
+    return this;
+  },
+
+  // configures the color of the circles in the chart.
+  // when called without arguments, returns the
+  // current color.
+  color: function(newColor) {
+    if (arguments.length === 0) {
+      return this.color;
+    }
+    this.color = newColor;
+    return this;
   }
 });
